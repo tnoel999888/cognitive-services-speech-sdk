861f56dfaf4a9e68bbd5068749d4f144ea2820cd
Timan Noel <tnoel7@msn.com>
2020-06-10 15:58:46 +0100

    Update converter.py

--- quickstart/javascript/node/from-file/converter.py
+++ quickstart/javascript/node/from-file/converter.py
@@ -1,22 +1,21 @@
 from utils import run_ffmpeg
 import sys
+import os
 
-params = { 'ar': 16000,
-           'ac': 1,
-           'ab': '16k',
-           'f': 'wav'
-         }
+# convert = sys.argv[2]
+
+params = { 
+'ar': 16000,
+'ac': 1,
+'ab': '16k',
+'f': 'wav'
+}
 
 options = {'--verbose': 1}
 
-# input_file = sys.argv[1]
-# output_file = sys.argv[2]
-# no_convert = sys.argv[3]
+input_file = sys.argv[1]
 
-input_file = './test4.m4a'
-output_file = './test4-conv.wav'
-
-# if(no_convert):
-#   return
+name = os.path.splitext(input_file)[0]
+output_file = "{name}-conv.wav".format(name=name)
 
 run_ffmpeg(input_file, output_file, params, options)
